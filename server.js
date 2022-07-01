4f1d734420a60766e45b02cc04067a5689b9b99e
ashrafibrahim96 <achraf.brahim@aiesec.net>
2022-07-01 17:51:11 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+const HOST = '127.0.0.1';
+
+app.get('/', (req,res) => {
+	res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+console.log(`Running on port ${PORT}`);
+});
+
+
+

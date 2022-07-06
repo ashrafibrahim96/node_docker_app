a68db1dbd06b01924c43155297bc8952ea1a649b
ashrafibrahim96 <achraf.brahim@aiesec.net>
2022-07-06 20:42:51 +0200

    testing

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+const PORT = 7000;
 const HOST = '127.0.0.1';
 
 app.get('/', (req,res) => {
-	res.send('Hello Everyone ! ');
+	res.send('Hello World');
 });
 
 app.listen(PORT, () => {
 console.log(`Running on port ${PORT}`);
 });
 
-
+module.exports = app
 
